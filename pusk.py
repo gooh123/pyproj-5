efcd6bde0afd330006c249692dfc8cf4b14a5808
gooh123 <101920016+gooh123@users.noreply.github.com>
2022-03-22 13:18:37 +0300

    Add files via upload

---
+++ pusk.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, send_from_directory, Blueprint
+# from functions import ...
+from main.views import main_blueprint
+from loader.views import loader_blueprint
+from utils import PostsHandler
+
+POST_PATH = "posts.json"
+UPLOAD_FOLDER = "uploads/images"
+
+app = Flask(__name__)
+
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+
+
+@app.route("/uploads/<path:path>")
+def static_dir(path):
+    return send_from_directory("uploads", path)
+
+
+@main_blueprint.route('/add')
+def add_page():
+    return render_template("post_list.html")
+
+
+app.run()
